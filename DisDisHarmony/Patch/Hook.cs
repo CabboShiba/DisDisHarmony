491c099dbc90551c26d4c2499f5bde492471598c
Cabbo <92642446+CabboShiba@users.noreply.github.com>
2023-02-17 20:50:53 +0100

    Add files via upload

---
+++ DisDisHarmony/Patch/Hook.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using System;
+using System.Runtime.InteropServices;
+
+namespace DisDisHarmony.Patch
+{
+    internal class Hook
+    {
+        [HarmonyPatch(typeof(Marshal), nameof(Marshal.ReadByte), new[] {typeof(IntPtr)})]
+        class HookMarshal
+        {
+            static bool Prefix(IntPtr ptr, ref byte __result)
+            {
+                try
+                {
+                    __result = 0;
+                }
+                catch(Exception ex)
+                {
+                    Program.Log("Error: " + ex.Message, "ERROR", ConsoleColor.Red);
+                }
+                return false;
+            }
+        }
+    }
+}
---
+++ DisDisHarmony/Program.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace DisDisHarmony
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Title = $"[{DateTime.Now}] DisDisHarmony made by https://github.com/CabboShiba";
+                object[] parameters = null;
+                var LoadedAssembly = Assembly.LoadFile(Path.GetFullPath(args[0]));
+                var paraminfo = LoadedAssembly.EntryPoint.GetParameters();
+                parameters = new object[paraminfo.Length];
+                Harmony patch = new Harmony("DisDisHarmony_https://github.com/CabboShiba");
+                patch.PatchAll(Assembly.GetExecutingAssembly());
+                LoadedAssembly.EntryPoint.Invoke(null, parameters);
+            }
+            catch (Exception ex)
+            {
+                Log("Error: " + ex.Message, "Error", ConsoleColor.Red);
+            }
+            Console.ReadLine();
+        }
+
+
+        public static void Log(string Data, string Type, ConsoleColor Color)
+        {
+            Console.ForegroundColor = Color;
+            Console.WriteLine($"[{DateTime.Now.ToString("HH:mm:ss")} - {Type}] {Data}");
+            Console.ResetColor();
+        }
+    }
+}
